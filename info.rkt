f3fdcd2bc57c63d2792c6fce81b64f0a2f6f4dc6
davidelettieri <davide.lettieri@gmail.com>
2023-02-25 15:26:38 +0100

    update info.rkt

--- info.rkt
+++ info.rkt
@@ -1,8 +1,8 @@
 #lang info
 (define collection "my-lang")
-(define deps '("base"))
+(define deps '("base" "parser-tools-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "parser-tools-lib"))
 (define scribblings '(("scribblings/my-lang.scrbl" ())))
 (define pkg-desc "Description Here")
-(define version "0.0")
-(define pkg-authors '(davide))
+(define version "0.0.1")
+(define pkg-authors '(davide lettieri))

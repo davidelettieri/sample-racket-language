b8728c5a6c4fc455676b7c9d748d96c420f40c28
davidelettieri <davide.lettieri@gmail.com>
2023-02-25 15:23:40 +0100

    update build deps

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "my-lang")
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "parser-tools-lib"))
 (define scribblings '(("scribblings/my-lang.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")

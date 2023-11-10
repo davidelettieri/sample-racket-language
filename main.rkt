b8249c42fdb5105746054cd921d9f327e54cfd2a
davidelettieri <davide.lettieri@gmail.com>
2023-11-10 09:36:23 +0100

    improve sample language

--- main.rkt
+++ main.rkt
@@ -1,18 +1,17 @@
 #lang racket
- 
+
 (module reader racket
   (require syntax/strip-context "lexer.rkt" "parser.rkt")
- 
+
   (provide (rename-out [my-read read]
                        [my-read-syntax read-syntax]))
 
   (define (my-read in)
     (syntax->datum
      (my-read-syntax #f in)))
- 
+
   (define (my-read-syntax src in)
     (with-syntax ([result (sample-parser (lambda () (sample-lexer in)))])
       (strip-context
-       #'(module anything racket
-           (require sample-racket-language/semantics)
+       #'(module anything sample-racket-language/expander
            result)))))
